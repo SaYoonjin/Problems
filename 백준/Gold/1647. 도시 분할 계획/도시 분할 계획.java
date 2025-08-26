92a7c7d76c961f1e760efe5fa83e956eb3ffc80b
SaYoonjin <tkdbswls1234@gmail.com>
2025-08-26 11:51:42 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1436 ms, Memory: 327320 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int[] P;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		List<edge> elist = new ArrayList<edge>();
+		
+		P = new int[V+1];
+		for (int i = 1; i <= V; i++) P[i] = i;
+		
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			edge m = new edge(a, b, c);
+			elist.add(m);
+		}
+		Collections.sort(elist);
+		
+		int result = 0, max = 0;
+		
+		for (edge e : elist) {
+			if (find(e.a) != find(e.b)) {
+				union(e.a, e.b);
+				result += e.w;
+				if (max < e.w) max = e.w;
+			}
+		}
+		
+		result -= max;
+		System.out.println(result);
+	}
+
+	public static void union(int a, int b) {
+		int p1 = find(a);
+		int p2 = find(b);
+		
+		if (p1 == p2) return;
+		
+		else if (p1 > p2) P[p1] = p2;
+		else P[p2] = p1;
+	}
+	
+	public static int find(int x) {
+		if (P[x] == x) return x;
+		else return P[x] = find(P[x]);
+	}
+	
+	public static class edge implements Comparable<edge>{
+
+		int a;
+		int b;
+		int w;
+
+		public edge(int a, int b, int w) {
+			super();
+			this.a = a;
+			this.b = b;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(edge o) {
+			return this.w - o.w;
+		}
+	}
+}

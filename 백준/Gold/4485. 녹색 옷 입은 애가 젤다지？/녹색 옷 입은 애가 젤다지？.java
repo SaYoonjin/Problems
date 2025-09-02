44fde02da6bd7fcbd630a7ff9a0119d18b891297
SaYoonjin <tkdbswls1234@gmail.com>
2025-09-02 14:21:10 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 220 ms, Memory: 18264 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,92 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static class Node{
+		int x, y, w;
 
+		public Node(int x, int y, int w) {
+			this.x = x;
+			this.y = y;
+			this.w = w;
+		}
+	}
+	
 	static int N;
-	static int[][] dist;
-	static int[][] nums;
-	static boolean[][] visited;
-	static Deque<Node> n;
-	static int[] dx = { 0, 0, 1, -1 };
-	static int[] dy = { 1, -1, 0, 0 };
-
+	static int[][] nums, dist;
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0 ,-1};
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
-		int tc = 1;
+		
 		N = Integer.parseInt(br.readLine());
-
-		while (true) {
-
-			n = new ArrayDeque<Node>();
-
-			dist = new int[N][N];
+		
+		int idx = 1;
+		
+		while (N != 0) {
+			sb.append("Problem " + idx + ": ");
+			idx++;
+			
 			nums = new int[N][N];
-			visited = new boolean[N][N];
-
+			dist = new int[N][N];
+		
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < N; j++) {
-					dist[i][j] = Integer.MAX_VALUE;
 					nums[i][j] = Integer.parseInt(st.nextToken());
+					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
-
 			dist[0][0] = nums[0][0];
-
-			int result = d(0, 0);
-
-			sb.append("Problem ").append(tc++).append(": ").append(result).append("\n");
-
+			sb.append(di()).append("\n");
 			N = Integer.parseInt(br.readLine());
-			if (N == 0) break;
 		}
 		System.out.println(sb);
 	}
-
-	public static int d(int x, int y) {
-		n.offer(new Node(x, y));
-
-		if (!visited[x][y]) visited[x][y] = true;
-
-		while (!n.isEmpty()) {
-			Node now = n.poll();
-
-			int a = now.x;
-			int b = now.y;
-
+	
+	public static int di() {
+		PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
+		q.add(new Node(0, 0, nums[0][0]));
+		
+		while (!q.isEmpty()) {
+			Node now = q.poll();
+			
 			for (int i = 0; i < 4; i++) {
-				int xx = a + dx[i];
-				int yy = b + dy[i];
-
-				if (xx < 0 || xx >= N || yy < 0 || yy >= N)
-					continue;
-
-				else {
-					if (dist[xx][yy] > dist[a][b] + nums[xx][yy]) {
-						dist[xx][yy] = dist[a][b] + nums[xx][yy];
-						n.offer(new Node(xx, yy));
-					}
+				int ax = dr[i] + now.x;
+				int ay = dc[i] + now.y;
+				
+				if (ax < 0 || ax >= N || ay < 0 || ay >= N) continue;
+				
+				if (dist[ax][ay] > dist[now.x][now.y] + nums[ax][ay]) {
+					dist[ax][ay] = dist[now.x][now.y] + nums[ax][ay];
+					q.add(new Node(ax, ay, dist[ax][ay]));
 				}
 			}
 		}
-		return dist[N - 1][N - 1];
+		return dist[N-1][N-1];
 	}
-
-	public static class Node {
-		int x;
-		int y;
-
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-    }
 }

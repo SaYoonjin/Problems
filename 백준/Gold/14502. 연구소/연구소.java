81203102af00572164847a422226e4a2d5df9edc
SaYoonjin <tkdbswls1234@gmail.com>
2025-09-12 14:01:32 +0900

    [Gold IV] Title: 연구소, Time: 896 ms, Memory: 301484 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,26 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	static int N, M;
+	static int max = 0, N, M;
+	static int[][] nums;
 	
 	public static void main(String[] args) throws IOException {
 		
 		// 0 : 빈 칸
 		// 1 : 벽
-		// 2 : 바이러스
+		// 2 : 바이러스가 있는 곳
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		int[][] nums = new int[N][M];
+		
+		nums = new int[N][M];
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -28,28 +26,26 @@ public class Main {
 				nums[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		int cnt = 0;
 		
-		backtracking(cnt, nums);
+		dfs(0, nums);
 		
 		System.out.println(max);
+		
 	}
 	
-	static int max = 0;
-	
-	public static void backtracking(int cnt, int[][] nums) {
+	public static void dfs(int cnt, int[][] nums) {
 		
 		if (cnt == 3) {
-			int safe = count(nums);
-			max = Math.max(safe, max);
-			return;
+			 int result = count(nums);
+			 if (result > max) max = result;
+			 return;
 		}
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				if (nums[i][j] == 0) {
 					nums[i][j] = 1;
-					backtracking(cnt + 1, nums);
+					dfs(cnt + 1, nums);
 					nums[i][j] = 0;
 				}
 			}
@@ -60,18 +56,18 @@ public class Main {
 	static int[] dc = {0, 1, 0, -1};
 	
 	public static int count(int[][] nums) {
-		boolean[][] visited = new boolean[N][M];
-		int cnt1 = 0;
 		
+		int[][] copy = new int[N][M];
 		Queue<int[]> q = new ArrayDeque<>();
+		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (nums[i][j] == 2) {
-					visited[i][j] = true;
-					q.add(new int[] {i, j});
-				}
+				copy[i][j] = nums[i][j];
+				if (copy[i][j] == 2) q.offer(new int[] {i, j});
 			}
 		}
+		
+		int c = 0;
 		
 		while (!q.isEmpty()) {
 			int[] now = q.poll();
@@ -81,20 +77,19 @@ public class Main {
 				int y = now[1] + dc[i];
 				
 				if (x < 0 || x >= N || y < 0 || y >= M) continue;
-				if (nums[x][y] == 1 || nums[x][y] == 2) continue;
 				
-				if (!visited[x][y]) {
-					visited[x][y] = true;
-					q.add(new int[] {x, y});
+				if (copy[x][y] == 0) {
+					copy[x][y] = 2;
+					q.offer(new int[] {x, y});
 				}
 			}
 		}
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (nums[i][j] == 0 && !visited[i][j]) cnt1++;
+				if (copy[i][j] == 0) c++;
 			}
 		}
-		return cnt1;
+		return c;
 	}
 }

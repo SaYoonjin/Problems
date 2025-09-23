05921ce21428257ebc867cfb3e194a476f728533
SaYoonjin <tkdbswls1234@gmail.com>
2025-09-23 15:20:57 +0900

    [Silver I] Title: IOIOI, Time: 248 ms, Memory: 20140 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		
+		int cnt = 0;
+		int pattern = 0;
+		
+		for (int i = 1; i < S.length()-1; i++) {
+			if (S.charAt(i-1) == 'I' && S.charAt(i) == 'O' && S.charAt(i+1) == 'I') {
+				pattern++;
+				if (pattern == N) {
+					cnt++;
+					pattern--;
+				}
+				i++;
+			} else pattern = 0;
+		}
+		
+		System.out.println(cnt);
+	}
+}

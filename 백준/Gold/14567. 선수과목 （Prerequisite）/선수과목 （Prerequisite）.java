f816c5f9b3b17f8281cb28ff180b590617c097cf
SaYoonjin <tkdbswls1234@gmail.com>
2025-10-28 20:25:11 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 552 ms, Memory: 128592 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,56 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
+		// 위상정렬 사용
+		
+		// 진입 차수가 0인 모든 노드를 큐에 넣음
+		// 큐에서 원소 꺼냄
+		
+		// N : 과목 수
+		// M : 선수 조건의 수
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		List<List<Integer>> graph = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
 		
-		int N = Integer.parseInt(st.nextToken()); // 과목 수
-		int M = Integer.parseInt(st.nextToken()); // 선수 조건의 수 
-		int[] result = new int[N+1];
-		
-		Deque<Integer> dq = new ArrayDeque<>();
-		int[] degree = new int[N+1];
-		
-		List<Integer>[] nums = new ArrayList[N+1];
-		for (int i = 0; i < N+1; i++) nums[i] = new ArrayList<>();
+		int[] indegree = new int[N+1];
 		
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			nums[a].add(b);
-			degree[b]++;
+			graph.get(a).add(b);
+			indegree[b]++;
 		}
 		
-		for (int i = 1; i < N+1; i++) {
-			if (degree[i] == 0) {
-				dq.offer(i);
-				result[i] = 1;
+		Queue<Integer> q = new LinkedList<>();
+		int[] semester = new int[N+1];
+		
+		for (int i = 0; i <= N; i++) {
+			if (indegree[i] == 0) {
+				q.offer(i);
+				semester[i] = 1;
 			}
 		}
 		
-		while (!dq.isEmpty()) {
-			int b = dq.poll();
-			for (int a : nums[b]) {
-				result[a] = Math.max(result[a], result[b]+1);	
-				degree[a]--;
-				if (degree[a] == 0) dq.offer(a);
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			
+			for (int next : graph.get(now)) {
+				indegree[next]--;
+				
+				if (indegree[next] == 0) q.offer(next);
+				semester[next] = Math.max(semester[next], semester[now]+1);
 			}
 		}
 		
-		for (int i = 1; i < result.length; i++) sb.append(result[i]).append(" ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			sb.append(semester[i]).append(" ");
+		}
 		System.out.println(sb);
+		
 	}
 }

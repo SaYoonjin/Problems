13d4e090c41cd45938fc5701640bc3d633fdb288
SaYoonjin <tkdbswls1234@gmail.com>
2026-04-02 22:30:14 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 236 ms, Memory: 20312 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,74 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static class Node{
-		int x, y, w;
-
-		public Node(int x, int y, int w) {
-			this.x = x;
-			this.y = y;
-			this.w = w;
-		}
-	}
 	
-	static int N;
 	static int[][] nums, dist;
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0 ,-1};
+	static int result, N;
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		int round = 1;
 		
-		N = Integer.parseInt(br.readLine());
-		
-		int idx = 1;
-		
-		while (N != 0) {
-			sb.append("Problem " + idx + ": ");
-			idx++;
+		while (true) {
+			N = Integer.parseInt(br.readLine());
+			
+			if (N == 0) break;
+			
+			sb.append("Problem " + round + ": ");
+			round ++ ;
 			
 			nums = new int[N][N];
 			dist = new int[N][N];
-		
-			for (int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < N; j++) {
+			result = 0;
+			
+			for (int i = 0; i < nums.length; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < nums.length; j++) {
 					nums[i][j] = Integer.parseInt(st.nextToken());
 					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			dist[0][0] = nums[0][0];
-			sb.append(di()).append("\n");
-			N = Integer.parseInt(br.readLine());
+			
+			dijk(0, 0);
+			
+			result = dist[N-1][N-1];
+			sb.append(result + "\n");
 		}
+		
 		System.out.println(sb);
+		
 	}
 	
-	public static int di() {
-		PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
-		q.add(new Node(0, 0, nums[0][0]));
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	static public void dijk(int a, int b) {
+		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[2]));
+		pq.offer(new int[] {a, b, nums[0][0]});
+		dist[0][0] = nums[0][0];
 		
-		while (!q.isEmpty()) {
-			Node now = q.poll();
+		while (!pq.isEmpty()) {
+			int[] now = pq.poll();
 			
 			for (int i = 0; i < 4; i++) {
-				int ax = dr[i] + now.x;
-				int ay = dc[i] + now.y;
+				int x = now[0] + dr[i];
+				int y = now[1] + dc[i];
 				
-				if (ax < 0 || ax >= N || ay < 0 || ay >= N) continue;
+				if (x < 0 || x >= N || y < 0 || y >= N) continue;
 				
-				if (dist[ax][ay] > dist[now.x][now.y] + nums[ax][ay]) {
-					dist[ax][ay] = dist[now.x][now.y] + nums[ax][ay];
-					q.add(new Node(ax, ay, dist[ax][ay]));
+				if (dist[now[0]][now[1]] + nums[x][y] < dist[x][y]) {
+					dist[x][y] = dist[now[0]][now[1]] + nums[x][y];
+					pq.offer(new int[] {x, y, dist[x][y]});
 				}
 			}
 		}
-		return dist[N-1][N-1];
 	}
 }

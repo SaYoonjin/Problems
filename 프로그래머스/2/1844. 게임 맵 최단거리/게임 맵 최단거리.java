40f9685020a39f938822a280b85ccf12ee6a4b63
SaYoonjin <tkdbswls1234@gmail.com>
2026-04-03 00:15:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.51 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,39 +2,36 @@ import java.util.*;
 
 class Solution {
     
+    static boolean[][] visited;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
-    static boolean[][] visited;
     
     public int solution(int[][] maps) {
         
-        visited = new boolean[maps.length][maps[0].length];
+        visited = new boolean[maps.length][maps[0].length]; // 5 x 5
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{0, 0});
         visited[0][0] = true;
+        maps[0][0] = 1;
         
         while (!q.isEmpty()){
             int[] now = q.poll();
             
+            if (now[0] == maps.length-1 && now[1] == maps[0].length-1) {
+                return maps[now[0]][now[1]];
+            }
+                        
             for (int i = 0; i < 4; i++){
                 int x = now[0] + dr[i];
                 int y = now[1] + dc[i];
                 
-                if (x == maps.length-1 && y == maps[0].length-1) {
-                    return maps[now[0]][now[1]] + 1;
-                }
-                
                 if (x < 0 || y < 0 || x >= maps.length || y >= maps[0].length) continue;
                 
-                if (!visited[x][y]) {
-                    if (maps[x][y] == 1){
-                        maps[x][y] = maps[now[0]][now[1]] + 1;
-                        visited[x][y] = true;
-                        q.offer(new int[]{x, y});
-                    }
+                if (!visited[x][y] && maps[x][y] == 1){
+                    visited[x][y] = true;
+                    q.offer(new int[]{x, y});
+                    maps[x][y] = maps[now[0]][now[1]] + 1;
                 }
-                
-                
             }
         }
         

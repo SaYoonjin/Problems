6701602b78ea258357a656ff5d1928ba6bb5b9ae
SaYoonjin <tkdbswls1234@gmail.com>
2026-04-04 23:51:49 +0900

    [level 2] Title: 배달, Time: 2.82 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 91.6 MB, 시간: 3.10 ms
+메모리: 88.7 MB, 시간: 2.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 03일 01:29:27
+2026년 04월 04일 23:51:46
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,29 +2,45 @@ import java.util.*;
 
 class Solution {
     
+    static int[] dist;
+    static ArrayList<Node>[] graph;
+    static PriorityQueue<Node> pq = new PriorityQueue<>();
+    
     static class Node implements Comparable<Node> {
+        
         int to, cost;
         
-        Node(int to, int cost){
+        Node(int to, int cost) {
             this.to = to;
             this.cost = cost;
         }
         
-        // 비용이 작은 노드부터 나온다
         @Override
-        public int compareTo(Node o){
+        public int compareTo(Node o) {
             return this.cost - o.cost;
         }
+        
     }
     
-    public int solution(int N, int[][] road, int K){
+    public int solution(int N, int[][] road, int K) {
         
-        ArrayList<Node>[] graph = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++){
+        // N : 마을 개수
+        // road : 도로 정보
+        // 음식 배달 가능 시간 : K
+        
+        // 출력 : 마을 개수
+        
+        
+        // 초기 설정 (dist, PriorityQueue)
+        dist = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<>();
+            dist[i] = Integer.MAX_VALUE;
         }
         
-        for (int[] r : road){
+        // 그래프 그리기
+        for (int[] r : road) {
             int a = r[0];
             int b = r[1];
             int c = r[2];
@@ -33,15 +49,25 @@ class Solution {
             graph[b].add(new Node(a, c));
         }
         
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        dijkstra();
         
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        int answer = 0;
+        
+        for (int i = 1; i < N + 1; i++) {
+            if (dist[i] <= K) answer++;
+        }
+        
+        return answer;
+        
+    }
+    
+    public static void dijkstra() {
         
         pq.offer(new Node(1, 0));
         dist[1] = 0;
         
-        while (!pq.isEmpty()){
+        while (!pq.isEmpty()) {
+            
             Node now = pq.poll();
             
             if (dist[now.to] < now.cost) continue;
@@ -50,19 +76,14 @@ class Solution {
                 
                 int newCost = dist[now.to] + next.cost;
                 
-                if (newCost < dist[next.to]){
+                if (dist[next.to] > newCost) {
                     dist[next.to] = newCost;
                     pq.offer(new Node(next.to, newCost));
                 }
+                
             }
-        }
-        int answer = 0;
-        
-        for (int i = 1; i <= N; i++){
-            if (dist[i] <= K) answer++;
+            
         }
         
-        return answer;
     }
-    
 }

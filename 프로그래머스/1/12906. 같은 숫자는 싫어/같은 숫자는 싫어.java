4d0876e469feff81cfa2092677ae9a591daab569
SaYoonjin <tkdbswls1234@gmail.com>
2026-04-28 10:06:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.93 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+
+        int prev = -1;
+
+        for (int num : arr) {
+            if (num != prev) {
+                list.add(num);
+                prev = num;
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}

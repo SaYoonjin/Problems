cae7caaa435591217b80cfbe8a9b90f26c452ea0
SaYoonjin <tkdbswls1234@gmail.com>
2026-05-18 17:28:50 +0900

    [level 2] Title: 구명보트, Time: 5.23 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        int answer = 0;
+        
+        while (left <= right) {
+            
+            // 가장 가벼운 사람 + 가장 무거운 사람이 같이 탈 수 있으면
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            
+            right--;
+            answer++;
+        }
+        
+        return answer;
+    }
+}

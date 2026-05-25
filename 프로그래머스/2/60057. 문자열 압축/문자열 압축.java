880248b3931aab1a7c94497abfccd8f360f5c9e4
SaYoonjin <tkdbswls1234@gmail.com>
2026-05-25 23:30:58 +0900

    [level 2] Title: 문자열 압축, Time: 0.99 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -3,47 +3,51 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         
+        // 자르는 단위수를 늘리면서 
+        // 1 ~ N/2
+        
+        // 지금 거 저장 
+        // 다음 거 비교하면서 cnt++ 하다가 달라지면 cnt 저장하고 0 초기화 & 지금 거 업데이트
+        // 전체 글자 수 다 센 뒤에 answer 랑 비교
+        
         int n = s.length();
+        int idx = 0;
         int answer = n;
         
-        for (int i = 1; i <= n / 2; i++) {
+        for (int i = 1; i <= n / 2; i++) {   // 자르는 단위 수
             
-            int idx = i, result = 0, cnt = 1;
-            String prev = s.substring(0, i);
-            
-            boolean stop = false;
-            
+            String now = s.substring(idx, idx + i);
+            idx += i;
+            int result = 0;
+            int cnt = 1;
+                        
             while (idx < n) {
                 
                 int end = Math.min(idx + i, n);
-                String now = s.substring(idx, end);
+                String next = s.substring(idx, end);        
                 
-                if (prev.equals(now)) {
+                if (now.equals(next)) {
                     cnt++;
+                    idx += i;
                 }
                 else {
-                    if (cnt > 1) result += String.valueOf(cnt).length();
-                    result += prev.length();
                     
-                    if (result >= answer) {
-                        stop = true;
-                        break;
-                    }
+                    if (cnt > 1) result += (String.valueOf(cnt).length() + i);
+                    else result += now.length();
                     
-                    prev = now;
+                    now = next;
+                    idx += i;
                     cnt = 1;
+                    
                 }
-                
-                idx += i;
                 
             }
             
-            if (stop) continue;
-            
             if (cnt > 1) result += String.valueOf(cnt).length();
-            result += prev.length();
+            result += now.length();
             
-            answer = Math.min(answer, result);
+            if (result < answer) answer = result;
+            idx = 0;
             
         }
         

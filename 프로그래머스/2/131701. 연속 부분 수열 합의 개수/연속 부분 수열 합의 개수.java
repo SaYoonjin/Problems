8eb6632590644a48bf44b0abe1e9bbb9094e880d
SaYoonjin <tkdbswls1234@gmail.com>
2026-07-04 15:43:48 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 61.03 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+        int n = elements.length;
+        
+        for (int start = 0; start < n; start++) {
+            int sum = 0;
+            
+            for (int len = 0; len < n; len++) {
+                sum += elements[(start + len) % n];
+                set.add(sum);
+            }
+        }
+        
+        return set.size();
+    }
+}

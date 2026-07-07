94e5c13f91c4c13401d88c0d816647a0ead37b56
SaYoonjin <tkdbswls1234@gmail.com>
2026-07-07 23:09:43 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,39 +3,38 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        int[] left = new int[progresses.length];
+        int[] time = new int[progresses.length];
         
         for (int i = 0; i < progresses.length; i++) {
             
-            int now = 100 - progresses[i];
-            
-            if (now % speeds[i] == 0) left[i] = now / speeds[i];
-            else left[i] = now / speeds[i] + 1;
+            int n = 100 - progresses[i];
+            if (n % speeds[i] == 0) time[i] = n / speeds[i];
+            else time[i] = n / speeds[i] + 1;
             
         }
         
-        List<Integer> days = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
-        int idx = 0;
+        int current = time[0];
+        int count = 1;
         
-        while (idx < left.length) {
+        for (int i = 1; i < time.length; i++) {
             
-            int n = left[idx++];
-            int cnt = 1;
-            
-            while (idx < left.length && left[idx] <= n) {
-                cnt++;
-                idx++;
+            if (time[i] <= current) {
+                count++;
+            } else {
+                list.add(count);
+                current = time[i];
+                count = 1;
             }
-
-            days.add(cnt);          
-            
         }
         
-        int[] answer = new int[days.size()];
+        list.add(count);
         
-        for (int i = 0; i < days.size(); i++) {
-            answer[i] = days.get(i);
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;

82951bd13a136d31553c903cce8b99a100a8cbfa
SaYoonjin <tkdbswls1234@gmail.com>
2026-07-09 23:25:25 +0900

    [level 2] Title: 프로세스, Time: 1.42 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,33 +3,39 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         
-        Queue<int[]> q = new ArrayDeque<>();
-        
-        for (int i = 0; i < priorities.length; i++) {
-            
-            q.offer(new int[] {priorities[i], i});
-            
-        }
+        // 큐에 int[] {우선순위, 위치} 저장 
+        // 하나 빼서 나머지랑 비교 -> 가장 크면 삭제 -> 삭제한 게 타겟이면 그 때 return 
+        //                      -> 아니면 삭제 후 재삽입 
         
         int answer = 0;
         
-        while (true) {
-            
-            boolean flag = false;
-            int[] now = q.poll();
-            
-            for (int[] item : q) {
-                if (item[0] > now[0]) {
-                    q.offer(now);
-                    flag = true;
-                    break;
-                }
-            }
-            
-            if(!flag && now[1] == location) return answer + 1;
-            else if (!flag) answer++;
-            
+        Queue<int[]> q = new ArrayDeque<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            return o2[0] - o1[0];
+        });
+        
+        int idx = 0;
+        for (int p : priorities) {
+            q.offer(new int[] {p, idx});
+            pq.offer(new int[] {p, idx++});
         }
-                
+        
+        while (!q.isEmpty()) {
+
+            int[] now = q.poll();
+            int[] m = pq.peek();
+
+            if (now[0] == m[0]) {
+                pq.poll();
+                answer++;
+
+                if (now[1] == location) return answer;
+            }
+            else q.offer(now);
+
+        }
+        
+        return answer;
+        
     }
 }
